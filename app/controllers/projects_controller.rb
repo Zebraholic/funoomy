af8d51a21013c7ed918c5bf1d3880aa8d2752f30
Zebraholic <rctavares@rogers.com>
2014-12-10 20:35:32 -0500

    Created views for user_sessions and new projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,48 @@
 class ProjectsController < ApplicationController
-  def index
+  
+def index
+  @projects = Project.all
   end
 
   def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to projects_url
+    else
+      render :new
+    end
   end
 
   def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+
+    if @project.update_attributes(project_params)
+      redirect_to project_path(@project)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+  end
+
+  
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name, :funding_goal, :description, :picture, :start_date, :end_date, rewards_attributes: [:id, :amount, :description, :_destroy])
   end
 end
