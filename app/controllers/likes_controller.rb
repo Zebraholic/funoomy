ffab8d117bd888e384ab4cb9a629ef71db7791c0
Zebraholic <rctavares@rogers.com>
2014-12-19 15:37:02 -0500

    Style changes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,27 +1,23 @@
 class LikesController < ApplicationController
   def index
-  	@user = current_user
+    @user = current_user
     @projects = Project.all
   end
 
-	def create
-	  project = Project.find(params[:project_id])
-	  Like.create(:project => project, :user => current_user)
-	  redirect_to project_path(project), :notice => "" 
+  def create
+    project = Project.find(params[:project_id])
+    Like.create(:project => project, :user => current_user)
+    redirect_to project_path(project), :notice => "" 
     respond_to do |format|
       format.js {}
       format.html {}
     end
-	end
+  end
 
-	def destroy
+  def destroy
     project = Project.find(params[:project_id])
     like = Like.find(params[:id])
     like.destroy
     redirect_to project_path(project), :notice => ""
   end
 end
-
-
-
-
